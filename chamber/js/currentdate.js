8ff81cda6db4dad88e00a440d6b5c3c19ffe7ba3
Zaanian <elderstephenstauffer@gmail.com>
2022-10-19 16:04:01 -0700

    update

--- chamber/js/currentdate.js
+++ chamber/js/currentdate.js
@@ -29,3 +29,7 @@ const monthName = monthnames[d.getMonth()];
 const year = d.getFullYear();
 const fullfooter = `${year} - Stephen Stauffer | ${dayName}, ${d.getDate()}, ${monthName}, ${year} | WA`;
 document.querySelector("#date").textContent = fullfooter;
+
+// message
+let message = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
+document.querySelector("#date-something").textContent = message;
