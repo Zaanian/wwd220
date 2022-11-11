80bb1b31c8322ad2b5755ac5ffdb4c06976bc3c3
Zaanian <elderstephenstauffer@gmail.com>
2022-11-11 11:44:31 -0800

    update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,17 +1,24 @@
 //JSON data
-const request = 'chamber/json/data.json'
 const cards = document.querySelector(".cards");
 
-
-fetch(request)
-  .then(function (response) {
+fetch("json/data.json").then(function (response) {
     return response.json();
-  })
-  .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const data = jsonObject['business'];
-    data.forEach(displaybusiness);
+}).then(function(jsonObject) {
+    console.table(jsonObject);  
+    const business = jsonObject['business'];
+    business.forEach(displayProphets);
   });
+
+  function displayProphets(business) {
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    
+    h2.textContent = `${business.name} `;
+
+    card.appendChild(h2);
+   
+    document.querySelector('div.cards').appendChild(card);
+  }
 
   
 
