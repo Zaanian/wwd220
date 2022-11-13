6e72ec92f7a36b694fbe292daf85114a7d27d6c9
Zaanian <elderstephenstauffer@gmail.com>
2022-11-12 16:26:38 -0800

    update

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -11,14 +11,54 @@ fetch("json/data.json").then(function (response) {
 
   function displayProphets(business) {
     let card = document.createElement('section');
+    let logo = document.createElement('img');
     let h2 = document.createElement('h2');
+    let address = document.createElement('span');
+    let phone = document.createElement('span');
+    let membershipLevel = document.createElement('span');
+    let website = document.createElement('a');
+
     
     h2.textContent = `${business.name} `;
+    address.textContent = `${business.address}`
+    website.textContent = `${business.website}`
+    phone.textContent = `${business.phonenumber}`
 
+    logo.setAttribute('src', business.logo);
+    logo.setAttribute('alt', `${business.name} logo`);
+    logo.setAttribute('loading', 'lazy');
+
+    website.setAttribute("href", business.website);
+    website.setAttribute("target", "_blank");
+
+    card.appendChild(logo);
     card.appendChild(h2);
+    card.appendChild(address)
+    card.appendChild(phone)
+    card.appendChild(website)
+
    
     document.querySelector('div.cards').appendChild(card);
   }
 
   
 
+//////// Grid View Toggle Function ///////////
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
