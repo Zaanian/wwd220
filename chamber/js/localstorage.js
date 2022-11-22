db3c6cb4e470b8af825bff7955f4b2d64215d33f
Zaanian <elderstephenstauffer@gmail.com>
2022-11-22 12:21:32 -0800

    update

--- chamber/js/localstorage.js
+++ chamber/js/localstorage.js
@@ -1,5 +1,5 @@
 
-const todayDisplay = document.querySelector(".today");
+const lastDisplay = document.querySelector(".last");
 const visitsDisplay = document.querySelector(".visits");
 
 
@@ -12,9 +12,22 @@ if (numVisits !== 0) {
 	visitsDisplay.textContent = `This is your first visit!`;
 }
 
-
 numVisits++;
 
 localStorage.setItem("visits-ls", numVisits);
 
+const lastVisit = Number(window.localStorage.getItem("last-visit"));
+console.log(lastVisit);
 
+if (lastVisit !== 0) {
+	const time = Date.now();
+
+	let difference = ((time - lastVisit) / (1000 * 3600 * 24)).toFixed(0);
+
+	localStorage.setItem("last-visit", time);
+
+	lastDisplay.textContent = difference;
+} else {
+	visitsDisplay.textContent = `Today is your first day here`;
+	localStorage.setItem("last-visit", Date.now());
+}
