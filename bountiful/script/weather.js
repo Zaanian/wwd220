3a40179280f279d4e07a58ceb65d565e780de1e8
Zaanian <elderstephenstauffer@gmail.com>
2022-12-13 15:18:05 -0800

    update

--- bountiful/script/weather.js
+++ bountiful/script/weather.js
@@ -1,21 +1,35 @@
+//Temp
 const currentTemp = document.querySelector('#current-temp');
+const DaytwoTemp = document.querySelector('#secondndtemp');
+const DayThreeTemp = document.querySelector('#thirdtemp');
+
+//Humidity
 const weatherhumidity = document.querySelector('#humidity');
+const weatherhumidity2 = document.querySelector('#humidity-second');
+const weatherhumidity3 = document.querySelector('#humidity-third');
+
+//Weather Icon
 const weatherIcon = document.querySelector('#weather-icon');
-const description = document.querySelector('.describe')
+const weatherIcon2 = document.querySelector('#weather-icon-second');
+const weatherIcon3 = document.querySelector('#weather-icon-third');
 
+//Description
+const description = document.querySelector('#describe');
+const description2 = document.querySelector('#describe-second');
+const description3 = document.querySelector('#describe-third');
 
-
-
-
-const link = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=70f32d8e36945d218801df44e122e1c2&units=imperial"
-const link2 = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&cnt=3&appid=70f32d8e36945d218801df44e122e1c2&units=imperial"
+//Links to openweather.org
+const link = "https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&appid=b5cf513fee5b29c16ccd6349eff084ae&cnt=12&units=imperial"
+const link2 = "https://api.openweathermap.org/data/2.5/weather?q=Carlsbad&appid=70f32d8e36945d218801df44e122e1c2&units=imperial"
+//----------------
 
 async function apiFetch(url) {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            displayResults(data)
+            displayDayTwo(data)
+            displayDayThree(data)
             console.log(data); 
         } else {
             throw Error(await response.text());
@@ -30,7 +44,7 @@ async function apiFetch2(url2) {
         const response = await fetch(url2);
         if (response.ok) {
             const data = await response.json();
-            displayResults2(data)
+            displayDayOne(data)
             console.log(data); 
         } else {
             throw Error(await response.text());
@@ -39,30 +53,45 @@ async function apiFetch2(url2) {
         console.log(error);
     }
 }
+//----------------------
 
-function displayResults(weatherData) {
+function displayDayOne(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    weatherhumidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(0)}</strong>`;
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
 
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     description.textContent = desc;
-    
 
 }
 
-function displayResults2(weatherData2) {
-    weatherhumidity.innerHTML = `<strong>${weatherData2.list[0].main.humidity.toFixed(0)}</strong>`;
-    const desc = weatherData2.weather[0].description;
+function displayDayTwo(weatherData) {
+    DaytwoTemp.innerHTML = `<strong>${weatherData.list[0].main.temp.toFixed(0)}</strong>`;
+    weatherhumidity2.innerHTML = `<strong>${weatherData.list[0].main.humidity.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
+    const desc = weatherData.list[0].weather[0].description;
 
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    description.textContent = desc;
+    weatherIcon2.setAttribute('src', iconsrc);
+    weatherIcon2.setAttribute('alt', desc);
+    description2.textContent = desc;
 
-    
+}
+
+function displayDayThree(weatherData) {
+    DayThreeTemp.innerHTML = `<strong>${weatherData.list[11].main.temp.toFixed(0)}</strong>`;
+    weatherhumidity3.innerHTML = `<strong>${weatherData.list[11].main.humidity.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[11].weather[0].icon}.png`;
+    const desc = weatherData.list[11].weather[0].description;
+
+    weatherIcon3.setAttribute('src', iconsrc);
+    weatherIcon3.setAttribute('alt', desc);
+    description3.textContent = desc;
 
 }
 
 apiFetch(link);
 apiFetch2(link2);
+
+
