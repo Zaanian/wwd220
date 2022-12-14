460499dc37f9132c3a9a838927ee2984979e9840
Zaanian <elderstephenstauffer@gmail.com>
2022-12-13 19:57:19 -0800

    update

---
+++ bountiful/script/fresh-order.js
@@ -0,0 +1,10 @@
+
+const number = (window.localStorage.setItem("numbtn", 0))
+
+const orderButton = document.getElementById("orderbtn")
+orderButton.onclick = checkingOrder
+
+function checkingOrder() {
+
+}
+console.log(localStorage)
--- bountiful/script/json-get.js
+++ bountiful/script/json-get.js
@@ -7,20 +7,53 @@ fetch(requestURL).then(function (response) {
     console.table(jsonObject);  
     const fruit = jsonObject 
     let arrayCount = 0
+    displayUnselected()
 while (arrayCount <= 38) {
   displaySelection(fruit, arrayCount);
   arrayCount++;
 }
+
+
   });
 
   //function
+  function displayUnselected() {
+    let nonselect1 = document.createElement("option");
+    let nonselect2 = document.createElement("option");
+    let nonselect3 = document.createElement("option");
+
+    nonselect1.setAttribute("name", "none")
+    nonselect1.textContent = `None*`
+
+    nonselect2.setAttribute("name", "none")
+    nonselect2.textContent = `None`
+
+    nonselect3.setAttribute("name", "none")
+    nonselect3.textContent = `None`
+
+    document.querySelector("#torque").append(nonselect1);
+    document.querySelector("#work").append(nonselect2);
+    document.querySelector("#play").append(nonselect3);
+
+
+
+  }
+
   function displaySelection(data, Count) {
-    let tesing = document.createElement("option")
+    let firstFruit = document.createElement("option");
+    let secondFruit = document.createElement("option");
+    let thirdFruit = document.createElement("option");
 
-    tesing.setAttribute("name", data[Count].name)
-    tesing.textContent = `${data[Count].name}`
+    firstFruit.setAttribute("name", data[Count].name);
+    firstFruit.textContent = `${data[Count].name}`;
 
-    document.querySelector("#torque").append(tesing)
-    document.querySelector("#work").append(tesing)
-    document.querySelector("#play").append(tesing)
+    secondFruit.setAttribute("name", data[Count].name);
+    secondFruit.textContent = `${data[Count].name}`;
+
+    thirdFruit.setAttribute("name", data[Count].name);
+    thirdFruit.textContent = `${data[Count].name}`;
+
+    document.querySelector("#torque").append(firstFruit);
+    document.querySelector("#work").append(secondFruit);
+    document.querySelector("#play").append(thirdFruit);
   }
